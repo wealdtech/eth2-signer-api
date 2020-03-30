89299b1724844c91428d88edc24aa8b9ddd69492
Jim McDonald <Jim@mcdee.net>
2020-03-30 15:03:38 +0100

    Update protobuf

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/wealdtech/eth2-signer-api
 go 1.13
 
 require (
-	github.com/golang/protobuf v1.3.4
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	github.com/golang/protobuf v1.4.0-rc.4
 	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
 	google.golang.org/grpc v1.28.0
+	google.golang.org/protobuf v1.20.1 // indirect
 )
--- pb/v1/generate.go
+++ pb/v1/generate.go
@@ -1,8 +1,8 @@
 package v1
 
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. accountmanager.proto
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. eth2.proto
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. lister.proto
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. responsestate.proto
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. signer.proto
-//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I /usr/local/include -I . --go_out=plugins=grpc:. walletmanager.proto
+//go:generate protoc -I . --go_out=. eth2.proto
+//go:generate protoc -I . --go_out=. responsestate.proto
+//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. accountmanager.proto
+//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. lister.proto
+//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. signer.proto
+//go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. walletmanager.proto
