3f6922a77be902a1a7c5dedfb9439883930e5bd9
Jim McDonald <Jim@mcdee.net>
2020-06-15 13:04:11 +0100

    Add distributed key generation service

--- pb/v1/generate.go
+++ pb/v1/generate.go
@@ -1,7 +1,8 @@
 package v1
 
-//go:generate protoc -I . --go_out=. dkg.proto
+//go:generate protoc -I . --go_out=plugins=grpc:. dkg.proto
 //go:generate protoc -I . --go_out=. eth2.proto
+//go:generate protoc -I . --go_out=. endpoint.proto
 //go:generate protoc -I . --go_out=. responsestate.proto
 //go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. accountmanager.proto
 //go:generate protoc -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I . --go_out=plugins=grpc:. lister.proto
